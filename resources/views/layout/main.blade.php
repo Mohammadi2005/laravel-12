4b39f57bc04d13a8e87ccd02247d3de36a7fd579
amir hossein <amohammadi161183@gmail.com>
2026-02-17 10:54:37 +0330

    layout

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>header for main layout</h1>
+        </nav>
+    </header>
+    <hr>
+
+    @yield("content")
+
+    <hr>
+    <footer>
+        <h1>footer for main layout</h1>
+    </footer>
+</body>
+</html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout.main')
+@section('content')
     <h1>show post {{$postId}}</h1>
-</body>
-</html>
+@endsection
